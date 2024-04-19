521ac58ac45a50fab8cbce31578d3d8a479373bf
bryce-h-t <bryce@cognition.ai>
2024-04-18 23:45:04 -0400

    W

--- stories/Header.jsx
+++ stories/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Button } from './Button';
+import Button from './Button';
 import './header.css';
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
@@ -57,3 +57,5 @@ Header.propTypes = {
 Header.defaultProps = {
   user: null,
 };
+
+export default Header;
